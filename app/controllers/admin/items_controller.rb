574625417f4190efa9136b9c6b3795e28800b28b
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-08-19 14:56:00 +0000

    [Add]商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,16 +1,31 @@
 class Admin::ItemsController < ApplicationController
   layout 'admin/layouts/application'  #layoutを宣言
-  
+
   def index
+    @item = Item.find(params[:id])
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
   
+  private
+  def item_params
+    params.require(:item).permit(:name,:introduction,:price,:genre_id)
+  end
 end
