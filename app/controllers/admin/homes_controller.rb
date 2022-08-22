73f0b11de0135bd1dc275eea211d1d092ea29d98
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-08-22 12:32:38 +0000

    [Add]注文履歴一覧画面

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,7 @@ class Admin::HomesController < ApplicationController
   layout 'admin/layouts/application'  #layoutを宣言
   
   def top
+    @ordering_details = OrderingDetail.page(params[:page])
   end
 
 end
