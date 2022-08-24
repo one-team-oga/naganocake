7c42128cbe5b0a5f558ddf9d7ce676dcff438a53
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-08-24 05:30:17 +0000

    [Add]商品詳細画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,17 @@
 class Public::CartItemsController < ApplicationController
   layout 'public/layouts/application'  #layoutを宣言
   def index
+    @cart_items = CartItem.all
+  end
+  
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.save
+    redirect_to cart_items_path
+  end
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,10 +3,12 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(8)
   end
-
+  
 
   def show
     @item = Item.find(params[:id])
+    @customer = current_customer
+    @cart_item = CartItem.new
   end
   
   private
