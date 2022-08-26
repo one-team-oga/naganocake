d61040be89a6fbc16ad03d110ed9b8a762a0ebad
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-08-26 03:25:54 +0000

    [Add]配送先一覧、編集
    、削除

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,36 @@
 class Public::AddressesController < ApplicationController
   layout 'public/layouts/application'  #layoutを宣言
   def index
+    @addresses = Address.all
+    @address = Address.new
+    @customer = current_customer
   end
-
+  
+  def create
+    address = Address.new(address_params)
+    address.save
+    redirect_to addresses_path
+  end
+  
   def edit
+    @address = Address.find(params[:id])
+    @customer = current_customer
+  end
+  
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
+  end
+  
+  def destroy
+   address = Address.find(params[:id])  
+   address.destroy
+   redirect_to addresses_path
+ end
+ 
+ private
+  def address_params
+    params.require(:address).permit(:name,:postal_code,:address,:customer_id)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :customer
     has_many :ordering_details, dependent: :destroy
+    
+    enum payment: { credit: 0, bank: 1 }
 end
