52388284cf9074d68af7cc4a4c06c1720ae57f4a
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-08-31 06:16:41 +0000

    [Add]顧客マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,11 @@
 class Public::CustomersController < ApplicationController
   layout 'public/layouts/application'  #layoutを宣言
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def unsubcribe
