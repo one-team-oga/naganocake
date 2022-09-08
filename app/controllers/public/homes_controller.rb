737e86667c000a0f469e909aa2596cb5b6eeefdb
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-09-08 15:25:27 +0000

    レイアウト調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,7 @@ class Public::HomesController < ApplicationController
   layout 'public/layouts/application'  #layoutを宣言
   def top
     @genres = Genre.all
+    @items = Item.order('id DESC').limit(4)
   end
 
   def about
